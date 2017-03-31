979c1d22d7af1d431a60863104d5128caf7030f7
William Mandai <wm@gitbench.com>
2017-04-01 01:32:38 +0300

    Added support for Slack and Sentry. Support for CI 3

--- application/config/monolog.php
+++ application/config/monolog.php
@@ -1,6 +1,7 @@
-<?php  if (!defined('BASEPATH')) {
-    exit('No direct script access allowed');
-}
+<?php
+ if (!defined('BASEPATH')) {
+     exit('No direct script access allowed');
+ }
 /*
  * CodeIgniter Monolog integration
  *
@@ -10,21 +11,20 @@
  * file that was distributed with this source code.
  */
 
-
 /* GENERAL OPTIONS */
-$config['handlers'] = array('file', 'new_relic', 'hipchat'); // valid handlers are file | new_relic | hipchat
+$config['handlers'] = array('file', 'slack', 'hipchat'); // valid handlers are file | new_relic | hipchat | sentry | slack
 $config['channel'] = ENVIRONMENT; // channel name which appears on each line of log
 $config['threshold'] = '4'; // 'ERROR' => '1', 'DEBUG' => '2',  'INFO' => '3', 'ALL' => '4'
-$config['introspection_processor'] = TRUE; // add some meta data such as controller and line number to log messages
+$config['introspection_processor'] = true; // add some meta data such as controller and line number to log messages
 
 /* FILE HANDLER OPTIONS
  * Log to default CI log directory (must be writable ie. chmod 757).
  * Filename will be encoded to current system date, ie. YYYY-MM-DD-ci.log
 */
-$config['file_logfile'] = APPPATH . 'logs/ci.log';
+$config['file_logfile'] = APPPATH.'logs/ci.log';
 
 /* NEW RELIC OPTIONS */
-$config['new_relic_app_name'] = 'App Name - ' . ENVIRONMENT;
+$config['new_relic_app_name'] = 'App Name - '.ENVIRONMENT;
 
 /* HIPCHAT OPTIONS */
 $config['hipchat_app_token'] = ''; //HipChat API Token
@@ -33,6 +33,14 @@ $config['hipchat_app_notification_name'] = 'Monolog'; //Name used in the "from" 
 $config['hipchat_app_notify'] = false; //Trigger a notification in clients or not
 $config['hipchat_app_loglevel'] = Logger::WARNING; //The minimum logging level at which this handler will be triggered
 
-
 // exclusion list for pesky messages which you may wish to temporarily suppress with strpos() match
 $config['exclusion_list'] = array();
+
+/* SLACK OPTIONS*/
+$config['slack_webhook'] = '{SLACK_WEBHOOK_URL}'; // Your slack webhook URL
+$config['slack_channel'] = '#{SLACK_CHANNEL}'; // Slack channel to send alerts
+$config['slack_username'] = '{SLACK_USERNAME}'; // Your slack username
+
+/* SENTRY OPTIONS */
+$config['raven_client'] = '{SENTRY_DSN_KEY}'; // Get it by logging in to your sentry account
+$config['raven_level'] = Logger::WARNING;
--- application/core/MY_Log.php
+++ application/core/MY_Log.php
@@ -1,7 +1,8 @@
-<?php if (!defined('BASEPATH')) {
+<?php
+
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
-
 /*
  * CodeIgniter Monolog integration
  *
@@ -11,21 +12,21 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 use Monolog\Logger;
 use Monolog\ErrorHandler;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\NewRelicHandler;
 use Monolog\Handler\HipChatHandler;
+use Monolog\Handler\SlackWebhookHandler;
+use Monolog\Handler\RavenHandler;
 use Monolog\Processor\IntrospectionProcessor;
 
 /**
- * replaces CI's Logger class, use Monolog instead
+ * replaces CI's Logger class, use Monolog instead.
  *
  * see https://github.com/stevethomas/codeigniter-monolog & https://github.com/Seldaek/monolog
- *
  */
-class CI_Log
+class MY_Log extends \CI_Log
 {
     // CI log levels
     protected $_levels = array(
@@ -33,53 +34,49 @@ class CI_Log
         'ERROR' => '1',
         'DEBUG' => '2',
         'INFO' => '3',
-        'ALL' => '4'
+        'ALL' => '4',
     );
-
     // config placeholder
     protected $config = array();
-
-
     /**
-     * prepare logging environment with configuration variables
+     * prepare logging environment with configuration variables.
      */
     public function __construct()
     {
         // copied functionality from system/core/Common.php, as the whole CI infrastructure is not available yet
-        if (!defined('ENVIRONMENT') OR !file_exists($file_path = APPPATH . 'config/' . ENVIRONMENT . '/monolog.php')) {
-            $file_path = APPPATH . 'config/monolog.php';
+        if (!defined('ENVIRONMENT') or !file_exists($file_path = APPPATH.'config/'.ENVIRONMENT.'/monolog.php')) {
+            $file_path = APPPATH.'config/monolog.php';
         }
-
         // Fetch the config file
         if (file_exists($file_path)) {
-            require($file_path);
+            require $file_path;
         } else {
             exit('monolog.php config does not exist');
         }
-
         // make $config from config/monolog.php accessible to $this->write_log()
         $this->config = $config;
-
         $this->log = new Logger($this->config['channel']);
         // detect and register all PHP errors in this log hence forth
         ErrorHandler::register($this->log);
-
         if ($this->config['introspection_processor']) {
             // add controller and line number info to each log message
             $this->log->pushProcessor(new IntrospectionProcessor());
         }
-
         // decide which handler(s) to use
         foreach ($this->config['handlers'] as $value) {
             switch ($value) {
                 case 'file':
                     $handler = new RotatingFileHandler($this->config['file_logfile']);
                     break;
-
                 case 'new_relic':
                     $handler = new NewRelicHandler(Logger::ERROR, true, $this->config['new_relic_app_name']);
                     break;
-
+                case 'slack':
+                    $handler = new SlackWebhookHandler($this->config['slack_webhook'], $this->config['slack_channel'], $this->config['slack_username']);
+                    break;
+                case 'sentry':
+                    $handler = new RavenHandler(new Raven_Client($this->config['raven_client']), $this->config['raven_level']);
+                    break;
                 case 'hipchat':
                     $handler = new HipChatHandler(
                         $config['hipchat_app_token'],
@@ -88,49 +85,42 @@ class CI_Log
                         $config['hipchat_app_notify'],
                         $config['hipchat_app_loglevel']);
                     break;
-                    
-                default:
-                    exit('log handler not supported: ' . $this->config['handler']);
-            }
 
+                default:
+                    exit('log handler not supported: '.$this->config['handler']);
+            }
             $this->log->pushHandler($handler);
         }
-
         $this->write_log('DEBUG', 'Monolog replacement logger initialized');
     }
-
-
     /**
-     * Write to defined logger. Is called from CodeIgniters native log_message()
+     * Write to defined logger. Is called from CodeIgniters native log_message().
      *
      * @param string $level
      * @param $msg
+     *
      * @return bool
      */
     public function write_log(
-        $level = 'error',
+        $level,
         $msg
     ) {
         $level = strtoupper($level);
-
         // verify error level
         if (!isset($this->_levels[$level])) {
-            $this->log->addError('unknown error level: ' . $level);
+            $this->log->addError('unknown error level: '.$level);
             $level = 'ALL';
         }
-
         // filter out anything in $this->config['exclusion_list']
         if (!empty($this->config['exclusion_list'])) {
             foreach ($this->config['exclusion_list'] as $findme) {
                 $pos = strpos($msg, $findme);
-
                 if ($pos !== false) {
                     // just exit now - we don't want to log this error
                     return true;
                 }
             }
         }
-
         if ($this->_levels[$level] <= $this->config['threshold']) {
             switch ($level) {
                 case 'ERROR':
@@ -145,6 +135,7 @@ class CI_Log
                     break;
             }
         }
+
         return true;
     }
 }
